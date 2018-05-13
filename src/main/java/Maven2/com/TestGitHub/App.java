85d97bf9d3967bf05a6208960d556d216ff2b9fc
Sidheshwar Tondare <sidheshwarpt@gmail.com>
2018-05-13 11:58:51 +0530

    initial commit

---
+++ src/main/java/Maven2/com/TestGitHub/App.java
@@ -0,0 +1,13 @@
+package Maven2.com.TestGitHub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Maven2/com/TestGitHub/LoginTest.java
@@ -0,0 +1,37 @@
+package Maven2.com.TestGitHub;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	public static WebDriver driver; 
+	
+	@BeforeTest
+	public void setUp(){
+		System.setProperty("webdriver.chrome.driver", "D:\\Somesh data\\SeleniumWebDriver\\OldVersionOfDrivers\\chromedriver2.36.exe");
+		driver=new ChromeDriver();
+		
+		
+	}
+	
+	@Test
+	public void doLogin(){
+		driver.get("https://www.facebook.com/");
+		driver.manage().timeouts().implicitlyWait(20L, TimeUnit.SECONDS);
+		driver.findElement(By.xpath(".//*[@id='email']")).sendKeys("9028549561");
+		driver.findElement(By.xpath(".//*[@id='pass']")).sendKeys("9604335555");
+	//	driver.findElement(By.xpath(".//*[@id='u_0_2']")).click();
+			}
+
+	@AfterTest
+	public void tearDown(){
+		driver.quit();
+		
+	}
+}
